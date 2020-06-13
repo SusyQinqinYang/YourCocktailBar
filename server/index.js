cb64879d2b8b2aa90d4127a5961c79740d3ca2d4
Susy(Qinqin) Yang <susyyang24@gmail.com>
2020-06-12 22:38:38 -0400

    trying to deploy

--- server/index.js
+++ server/index.js
@@ -5,4 +5,7 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '../dist/')))
 
+app.get('/#', (request, response) => {
+	response.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 app.listen(PORT, () => console.log(`Server listening on ${PORT}!`));
